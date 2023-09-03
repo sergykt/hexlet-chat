f8bba8000ba30ee9eb732c974d9af84588273c7e
sergykt <sergykt1992@mail.ru>
2023-09-04 01:13:25 +0600

    fixed routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const apiPath = 'https://hexlet-chat-fastify.onrender.com/api/v1';
+const apiPath = process.env.NODE_ENV === 'production' ? 'https://hexlet-chat-fastify.onrender.com/api/v1' : '/api/v1';
 
 const routes = {
   loginPath: () => [apiPath, 'login'].join('/'),
