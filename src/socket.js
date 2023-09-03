897cdfff8a49f2b669e97cba297406f5505144cb
sergykt <sergykt1992@mail.ru>
2023-09-04 00:08:31 +0600

    fixed socket io

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
+const URL = process.env.NODE_ENV === 'production' ? 'https://hexlet-chat-fastify.onrender.com/' : 'http://localhost:3000';
 
 const socket = io(URL);
 
